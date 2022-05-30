aa4d2b35ba236b8d423b36baed42cf84fe6e4a99
EyobABN <eyob.alamerew.belay@gmail.com>
2022-05-30 20:53:26 +0300

    Add 10-check_cycle.c and lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a cycle exists in a linked list
+ * @list: the head of the linked list
+ *
+ * Return: 1 if there is a cycle, 0 if there isn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *f = list, *s = list;
+
+	if (list == NULL)
+		return (0);
+	while (1)
+	{
+		f = f->next->next;
+		s = s->next;
+		if (f == NULL || s == NULL)
+			return (0);
+		if (f == s)
+			break;
+	}
+	return (1);
+}
