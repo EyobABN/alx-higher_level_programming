ff5b868e3b41904a82b308508e815c2311b35d99
EyobABN <eyob.alamerew.belay@gmail.com>
2022-05-31 16:46:38 +0300

    Remove the extra space in the separator

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ separator = ""
 for i in range(10):
     for j in range(10):
         if (i < j):
-            print(separator, "{}{}".format(i, j), end="")
+            print(separator, "{}{}".format(i, j), sep="", end="")
             separator = ", "
 print()
