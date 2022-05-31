1aab1203ff37bef0b5901b1b94c71306fc831b8a
EyobABN <eyob.alamerew.belay@gmail.com>
2022-05-31 14:07:36 +0300

    Rework handling for negative numbers and zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
+digit = round(math.fmod(number, 10))
 if digit > 5:
     place = "greater than 5"
 elif digit == 0:
-    place = "zero"
+    place = "0"
 else:
     place = "less than 6 and not 0"
 print(f"Last digit of {number} is {digit} and is {place}")
