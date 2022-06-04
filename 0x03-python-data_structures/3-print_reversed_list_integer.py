eb90282c0d0cd62493b37c24e73aef02e81efa70
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-04 23:23:18 +0300

    Specify a newline character as the end

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     rev_list = my_list.copy()
     rev_list.reverse()
     for element in rev_list:
-        print("{:d}".format(element))
+        print("{:d}".format(element), end="\n")
