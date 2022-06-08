7e01135b67b77e5e9e1de47313816b30cb6281ba
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-08 20:40:06 +0300

    Work on a copy of the original matrix instead

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,4 @@
 #!/bin/usr/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda num: num ** 2, row)), matrix))
+    return list(map(lambda row: list(map(lambda num: num ** 2, row)),
+                    matrix.copy()))
