9b261afe787d64fe207f211fb076fb35eb7e734b
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-08 16:08:01 +0300

    Handle exception for when no score is found

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    score_exists = False
+    for key in a_dictionary:
+        if a_dictionary[key]:
+            score_exists = True
+    if score_exists is False:
+        return None
     return max(a_dictionary, key=a_dictionary.get)
