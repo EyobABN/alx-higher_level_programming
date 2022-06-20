a3a17ede67be44da185db810f339806c953f31dd
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-20 22:34:22 +0300

    Add 100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as e:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(str(e))
+        sys.stderr.write("\n")
+        return False
+    except ValueError as e:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(str(e))
+        sys.stderr.write("\n")
+        return False
