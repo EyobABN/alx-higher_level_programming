200e57d2a3fc19cded2f28846a7ba0cc0ed1a5c2
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-20 22:46:50 +0300

    Generalize exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,22 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except IndexError as e:
-        sys.stderr.write("Except: ")
-        sys.stderr.write(str(e))
-        sys.stderr.write("\n")
-        return None
-    except TypeError as e:
-        sys.stderr.write("Except: ")
-        sys.stderr.write(str(e))
-        sys.stderr.write("\n")
-        return None
-    except ValueError as e:
-        sys.stderr.write("Except: ")
-        sys.stderr.write(str(e))
-        sys.stderr.write("\n")
-        return None
-    except ZeroDivisionError as e:
+    except Exception as e:
         sys.stderr.write("Except: ")
         sys.stderr.write(str(e))
         sys.stderr.write("\n")
