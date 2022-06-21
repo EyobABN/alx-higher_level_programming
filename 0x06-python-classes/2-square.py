eba29ecb2585669705a74bc4fc58e57e87286194
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-22 00:01:51 +0300

    Add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module for task 0 of 0x06-Python-classes project -
+write an empty class Square that defines a square """
+
+
+class Square:
+    """ an empty class that defines a square """
+    def __init__(self, size=0):
+        """ initializer function for instantiated object(s) """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
