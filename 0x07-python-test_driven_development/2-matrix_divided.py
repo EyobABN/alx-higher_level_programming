f81863c06b2222695205dcc5a477a146c103c4c7
EyobABN <eyob.alamerew.belay@gmail.com>
2022-06-29 01:24:15 +0300

    Add 2-matrix_divided.py and tests/2-matrix_divided.txt

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    module for dividing all elements of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+        function that divides a matrix
+    """
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if not type(matrix) is list or len(matrix) == 0:
+        raise TypeError(msg)
+    row_size = len(matrix[0])
+    for row in matrix:
+        if not type(row) is list:
+            raise TypeError(msg)
+        for cell in row:
+            if not (type(cell) is int or type(cell) is float):
+                raise TypeError(msg)
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+    if not (type(div) is int or type(div) is float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_row = []
+        for j in range(len(matrix[i])):
+            new_row.append(round(matrix[i][j] / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
