720319eb20c17126b26b6cde2138a84463cb2925
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-04 21:12:44 +0300

    Add 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module for same class of inherit frm
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        function for same class or inherit frm
+    """
+    return isinstance(obj, a_class)
