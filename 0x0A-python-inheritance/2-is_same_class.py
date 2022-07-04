403582ddf58135d836842a4da4707925ca5abd86
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-04 20:46:55 +0300

    Add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module for checking class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        function that checks the class of an object
+    """
+    return isinstance(obj, a_class)
