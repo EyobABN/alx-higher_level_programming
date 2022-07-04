edc917db5a4ab6847ed5ffcee8643204b121f3ea
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-04 21:39:02 +0300

    Another try

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """
         function for inheritance
     """
-    return issubclass(obj.__class__, a_class) and a_class is not bool
+    return issubclass(type(obj), a_class)
