9562527df401b3f9a6c1c9dfce828adf65edb21b
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-04 21:17:34 +0300

    Add 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module for inheritance
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+        function for inheritance
+    """
+    return isinstance(obj, a_class)
