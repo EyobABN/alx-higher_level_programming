c985cb8645edf25a76d4b900feda21754cae59eb
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-05 21:08:53 +0300

    Rename 2-append_file.py to 2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module for appending string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+        function for appending string to a text file
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
