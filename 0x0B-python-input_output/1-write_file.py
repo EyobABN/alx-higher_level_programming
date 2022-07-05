36e3256d63ed1230d877e5514aadc507e8c1cbb3
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-05 21:01:32 +0300

    Add 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module for writing a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        function for writing a string to a text file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
