508d09f59ada3b4175907d6e9e89d5d8159d847e
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-05 20:46:49 +0300

    Add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module for reading a text file and printing to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+        a function that reads a text file and prints it to stdout
+    """
+    with open(filename) as f:
+        print(f.read())
