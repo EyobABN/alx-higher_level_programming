3235fda8b6ec313ecd26c4c6865de1b65ec09383
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-05 21:16:25 +0300

    Set return statement

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
         function for writing a string to a text file
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text + '\n')
+        return (f.write(text + '\n'))
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,4 +9,4 @@ def append_write(filename="", text=""):
         function for appending string to a text file
     """
     with open(filename, 'a', encoding="utf-8") as f:
-        f.write(text)
+        return (f.write(text))
