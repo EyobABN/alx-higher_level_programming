b4c579676367bf0afce569d70ad884df55467b3f
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-05 20:56:41 +0300

    Do nothing if nothing passed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,7 +9,8 @@ def read_file(filename=""):
         a function that reads a text file and prints it to stdout
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        content = f.read()
-        if content[-1] == '\n':
-            content = content[:-1]
-        print(content)
+        if filename:
+            content = f.read()
+            if content[-1] == '\n':
+                content = content[:-1]
+            print(content)
