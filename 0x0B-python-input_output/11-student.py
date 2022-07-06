e19f942630e9e9d700116880742f0143a34edaa1
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-06 10:35:04 +0300

    Add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    module for Student to JSON
+"""
+
+
+class Student:
+    """
+        class for Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            retrieves a dictionary representation of a Student instance
+        """
+        if bool(attrs) and all(isinstance(elem, str) for elem in attrs):
+            dct = {}
+            for elem in attrs:
+                if hasattr(self, elem):
+                    dct[elem] = self.__getattribute__(elem)
+        else:
+            dct = self.__dict__
+        return dct
+
+    def reload_from_json(self, json):
+        """
+            replaces all attributes of the Student instance with json
+        """
+        self.__dict__ = json
