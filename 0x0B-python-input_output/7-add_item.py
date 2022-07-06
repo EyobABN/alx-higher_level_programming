4203455692559b3fcdd6a9ffba2d4124b1de0c53
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-06 09:16:14 +0300

    Fix truncating file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    lst = []
+    try:
+        with open('add_item.json', 'r', encoding="utf-8") as f:
+            lst = json.load(f)
+    except Exception:
+        lst = []
     for av in sys.argv[1:]:
         lst.append(av)
     with open('add_item.json', 'w+', encoding="utf-8") as f:
