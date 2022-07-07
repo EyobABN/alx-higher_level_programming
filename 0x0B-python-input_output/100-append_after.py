a3e5b37980ad51df20d175d0a382bb4c0a126cd5
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-07 09:53:16 +0300

    Add 100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    module for append after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+        function for append after
+    """
+    with open(filename, 'r+', encoding="utf-8") as f:
+        contents = f.readlines()
+
+    for line in range(len(contents)):
+        if search_string in contents[line]:
+            contents.insert(line + 1, new_string)
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        contents = "".join(contents)
+        f.write(contents)
