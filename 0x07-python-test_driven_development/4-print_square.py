0b02e7acefc0c313475ef9d83ddfe7d02730f51f
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-08 11:39:28 +0300

    Add task 4 files

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    moduler for printing a square with the character #
+"""
+
+
+def print_square(size):
+    """
+        function for printing a square of #'s
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
