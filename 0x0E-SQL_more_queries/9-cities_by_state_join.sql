b40c6117d8644213759c3637a71ceef63d56d300
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 22:36:43 +0300

    Use inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM states NATURAL JOIN cities ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON cities.state_id = states.id ORDER BY cities.id ASC;
