987d632486e01020f93910e8fe8a40cd6e360e81
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 22:33:22 +0300

    Asc order

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM states NATURAL JOIN cities;
+SELECT cities.id, cities.name, states.name FROM states NATURAL JOIN cities ORDER BY cities.id ASC;
