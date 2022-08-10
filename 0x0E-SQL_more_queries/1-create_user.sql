c532fe968825dc2101a224e79599bddac0fb71ef
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 19:55:00 +0300

    localhost, not just host

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates user_0d_1 and gives it privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'host' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'host';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 
