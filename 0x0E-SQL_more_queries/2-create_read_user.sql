f09412a7d828a390da2c6bf12fb9203225b450dc
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 20:06:51 +0300

    Fix password for user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
 
 -- grant select privilege
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
