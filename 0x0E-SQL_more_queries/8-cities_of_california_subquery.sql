fb289bd5962ca759b501de1af7705d2cb5fd4827
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 20:56:50 +0300

    Revert last change, and capitalize keywords

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of california that can be found in the hbtn_0d_usa database
-SELECT id, name FROM cities WHERE state_id = (SELECT state_id from states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
