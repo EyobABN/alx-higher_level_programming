db39d0ac73c30d3a7d8898d93d51c3cc00e7a819
EyobABN <eyob.alamerew.belay@gmail.com>
2022-08-10 20:54:08 +0300

    pick state id from states, not id, which doesn't exist

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of california that can be found in the hbtn_0d_usa database
-SELECT id, name FROM cities WHERE state_id = (SELECT id from states WHERE name = 'California') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT state_id from states WHERE name = 'California') ORDER BY id ASC;
