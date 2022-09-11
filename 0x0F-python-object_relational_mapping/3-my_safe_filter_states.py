f559d325742208cecefe3d146def45d83867dd38
EyobABN <eyob.alamerew.belay@gmail.com>
2022-09-11 21:04:48 +0300

    Fix string formatting typo

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    takes in an argument and displays all values in the states table of
-    hbtn_0e_0_usa where name matches the argument
+    python script that lists all states from the database hbtn_0e_0_usa with a
+    given name and is safe from MySQL injections
 """
 
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY \
-    id ASC".format(argv[4],))
+    id ASC", (argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
