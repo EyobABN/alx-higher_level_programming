1a756da84bafc92c310a09fd6073ad2a8ef49a96
EyobABN <eyob.alamerew.belay@gmail.com>
2022-09-11 22:12:53 +0300

    Fix import sourcing

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,7 +6,7 @@
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
