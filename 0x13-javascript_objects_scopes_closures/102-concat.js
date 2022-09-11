bf5c2fe5faaf3a0b42540540d073a4c5a2a55de4
EyobABN <eyob.alamerew.belay@gmail.com>
2022-09-11 15:03:28 +0300

    Add task 13 file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = fs.readFileSync(process.argv[2], 'utf8', function (err, result) { if (err) console.log('error', err); });
+const fileB = fs.readFileSync(process.argv[3], 'utf8', function (err, result) { if (err) console.log('error', err); });
+
+const sum = fileA.concat(fileB);
+
+fs.writeFile(process.argv[4], sum, 'utf8', function (err, result) { if (err) console.log('error', err); });
