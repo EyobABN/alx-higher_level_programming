ee2443a2679ef32d64f7035a8cd175804ce06547
EyobABN <eyob.alamerew.belay@gmail.com>
2022-09-11 21:45:21 +0300

    Add task 12 file

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    prints the first State object from the database hbtn_0e_6_usa
+"""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                                        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
+    satte.name = "New Mexico"
+    session.commit()
+    session.close()
