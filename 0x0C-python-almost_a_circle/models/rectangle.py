5697d71faf742df0d2de2eac92da4526474c9098
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-12 14:16:58 +0300

    Add input validation for Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be an integer')
         self.__width = width
 
     @property
@@ -30,6 +34,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be an integer')
         self.__height = height
 
     @property
@@ -38,6 +46,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = x
 
     @property
@@ -46,4 +58,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
         self.__y = y
