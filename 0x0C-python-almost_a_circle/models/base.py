71447bb036f2ee8504204b6b40bb2d79dbd271be
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-12 13:55:53 +0300

    use Base instead of self.__class__

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,8 +11,13 @@ class Base:
     __nb_objects = 0
     
     def __init__(self, id=None):
+        """Initializes a new polygon object with the given id.
+
+        Args:
+            id (int): The id of this polygon object.
+        """
         if id is not None:
             self.id = id
         else:
-            self.__class__.__nb_objects += 1
-            self.id = self.__class__.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
