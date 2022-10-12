a7cef89955b4a2ae8f1f6937df508c793f54072a
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-12 15:18:47 +0300

    Fix syntax for calling super()

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 def Square(Rectangle):
     """Square class that inherits from the Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.__super__.__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
