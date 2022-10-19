cf874d496b7d4dbe97646709e18a6e68d05d204c
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-19 06:55:21 +0300

    Refactor

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,16 +3,7 @@ const fs = require('fs');
 const request = require('request');
 
 if (process.argv.length > 3) {
-  request(`${process.argv[2]}`, (err, res, body) => {
-    if (err) {
-      console.log(err);
-    } else if (body) {
-      const contents = body.toString('utf-8');
-      fs.writeFile(process.argv[3], contents, err => {
-        if (err) {
-          console.log(err);
-        }
-      });
-    }
-  });
+  request
+    .get(`${process.argv[2]}`)
+    .pipe(fs.createWriteStream(process.argv[3]));
 }
